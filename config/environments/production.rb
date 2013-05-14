8559f43cd63520134dc93d62fd40c24d1f801d9d
Fabian Aguirre <faguirre@nettheory.com>
2013-05-13 21:48:39 -0500

    Changed production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Foodcon::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
+  # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
@@ -10,11 +10,11 @@ Foodcon::Application.configure do
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -22,20 +22,12 @@ Foodcon::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  # Raise exception on mass assignment protection for Active Record models
-  #config.active_record.mass_assignment_sanitizer = :strict
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  #config.active_record.auto_explain_threshold_in_seconds = 0.5
-
   # Do not compress assets
-  config.assets.compress = false
+  config.assets.compress = true
 
-  # Expands the lines which load the assets
-  config.assets.debug = true
   config.active_support.deprecation = :notify
-  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:enable_starttls_auto => false}
   config.middleware.use ExceptionNotifier,
     :email_prefix => "[Foodcon Production] ",
     :sender_address => %{"notifier" <notifier@example.com>},
