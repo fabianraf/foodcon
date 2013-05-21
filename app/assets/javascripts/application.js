ba6d78c66b5d516ffcfee59429d82bf16ff7b1e5
Fabian Aguirre <faguirre@nettheory.com>
2013-05-20 22:59:43 -0500

    Added min

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,5 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//
 //= require foundation
 $(document).foundation();
